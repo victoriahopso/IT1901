58d3370017fb705e77b3e1aba813f3f5dbd32161
Victoria Hopsø <victoho@stud.ntnu.no>
2020-09-11 15:53:33 +0000

    updated app.java to launch gui

--- 03/src/main/java/it1901/App.java
+++ 03/src/main/java/it1901/App.java
@@ -1,13 +1,29 @@
 package it1901;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+public class App extends Application {
+    
+  @Override
+  public void start(final Stage primaryStage) throws Exception {
+    final Parent parent = FXMLLoader.load(getClass().getResource("MyMovies.fxml"));
+    primaryStage.setScene(new Scene(parent));
+    primaryStage.show();
+  }
+
+  
+  public static void main(final String[] args) {
+    launch(args);
+  }
+
 }
