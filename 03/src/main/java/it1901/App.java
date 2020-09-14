b3d838a49153eeab8f1d16904d8aa1b27e895cb1
victoriahopso <victoriahopso@dhcp-10-22-12-164.wlan.ntnu.no>
2020-09-14 11:16:34 +0200

    Made controller class

--- 03/src/main/java/it1901/App.java
+++ 03/src/main/java/it1901/App.java
@@ -3,6 +3,7 @@ package it1901;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,12 @@ public class App extends Application{
 
 	@Override
     public void start(final Stage primaryStage) throws Exception {
-        primaryStage.setTitle("MyMovies");
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MyMovies.fxml"))));
-        primaryStage.show();
+	    final Parent parent = FXMLLoader.load(getClass().getResource("MyMovies.fxml"));
+	    primaryStage.setScene(new Scene(parent));
+	    primaryStage.show();
+//        primaryStage.setTitle("MyMovies");
+//        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MyMovies.fxml"))));
+//        primaryStage.show();
     }
 
     public static void main(final String[] args) {
