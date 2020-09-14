74dbf7b550c28aae889ea50b1c38e68473ffc744
August Andreas Lykke <augustal@stud.ntnu.no>
2020-09-14 10:36:23 +0000

    Laget film-klassen og pusher rett til master, grunnet feil ved merge

---
+++ 03/src/main/java/it1901/Film.java
@@ -0,0 +1,49 @@
+package it1901;
+
+
+public class Film{
+    private String name, genre, comment;
+    private int rating;
+
+    public Film(String name, String genre, int rating, String comment){
+        this.name=name;
+        this.genre=genre;
+        this.rating=rating;
+        this.comment=comment;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getGenre(){
+        return this.genre;
+    }
+    public int getRating(){
+        return this.rating;
+    }
+    public String getComment(){
+        return this.comment;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setGenre(String genre){
+        this.genre=genre;
+    }
+    public void setRating(int rating){
+        this.rating=rating;
+    }
+    public void setComment(String comment){
+        this.comment=comment;
+    }
+
+    public String toString(){
+        return this.name + " " + this.genre + " " + Integer.toString(this.rating) + " " + this.comment;
+    }
+
+
+
+}
