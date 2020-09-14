3810e979e312b207e5af65cd662bee8e5f5231e0
Victoria Hopsø <victoho@stud.ntnu.no>
2020-09-14 11:25:36 +0000

    Oppdatert referanser til Film og MyMovies

--- 03/src/main/java/it1901/MyMoveisController.java
+++ 03/src/main/java/it1901/MyMoveisController.java
@@ -1,7 +1,10 @@
 package it1901;
 
+import java.io.FileNotFoundException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -11,9 +14,9 @@ import javafx.scene.control.TextField;
 
 public class MyMoveisController {
 	
-	private ObservableList<String> ratings = FXCollections.observableArrayList("1", "2", "3", "4", "5");
+	private ObservableList<String> ratings = FXCollections.observableArrayList("1", "2", "3", "4", "5","6");
 	private ObservableList<String> genres = FXCollections.observableArrayList("Horror", "Comedy", "Romantic", "Action", "Thriller", "Sci-fi");
-	
+	private MyMovies movs = new MyMovies();
 	
 	@FXML
 	private Button submit;
@@ -31,22 +34,36 @@ public class MyMoveisController {
 			genre.setItems(genres);
 			rating.setItems(ratings);	
 		}
-	 
+     
+
 	@FXML
 	private void handleSubmit() {
 		if (validTitle() && isRated() && genreChosen()) {
-			//save details
-			submitted();
+			Film film = new Film (title.getText(), genre.getValue(), Integer.valueOf(rating.getValue()), comment.getText());
+            submitted();
+            //creates new film that automaticly adds it self to a container, MyMovies. 
+            //container content will be saved to file when program is exited. 
 		}
 		else {
 			message.setText("Please enter title, rating and genre before submitting");
 		}
-	}
+    }
+    
+    //This method handles saving to file. 
+    //At this iteration, loading from file is not yet implemented
+    //so method might seem pointless.
+    //will later expand with loading.
+    @FXML
+    private void exitApp(ActionEvent event) throws FileNotFoundException {
+    	RWFile fil = new RWFile();
+    	fil.fileWriter("boardObject.txt", movs);
+        System.exit(0);
+    }
 	
 	private boolean validTitle() {
 		return (!title.getText().equals(""));
 		//this method should later include more sufficient 
-		//validation of a valid movie title. 
+        //validation for a valid movie title. 
 	}
 	private boolean isRated() {
 		return rating.getValue()!=null;
