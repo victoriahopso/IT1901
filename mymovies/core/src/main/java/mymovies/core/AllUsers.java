f4d85034200e3a8d6c8eeb3bbfcbd0f2fcf7d01f
August Andreas Lykke <augustal@stud.ntnu.no>
2020-11-10 20:48:32 +0000

    Fjernet unødvendig kode i allUsers

--- mymovies/core/src/main/java/mymovies/core/AllUsers.java
+++ mymovies/core/src/main/java/mymovies/core/AllUsers.java
@@ -7,27 +7,20 @@ import java.util.Iterator;
 public class AllUsers implements Iterable<User> {
 
   /**
-   * Det skal kun eksistere én av denne klassen. Den skal lagres i skyen. Den
-   * holder styr på brukere. En kan ikke opprette en bruker med samme brukernavn
-   * som en av brukerene i denne klassen.
+   * Det skal kun eksistere én av denne klassen. Den skal lagres i skyen. Den holder styr på brukere.
+   * En kan ikke opprette en bruker med samme brukernavn som en av brukerene i denne klassen.
    */
 
   private Collection<User> users = new ArrayList<>();
 
   public User addUser(User user) {
-    if (!users.contains(user)) {
-      int counter = 0;
-      for (User us : users) {
-        if (!us.getUserName().equals(user.getUserName())) {
-          counter++;
-        }
-      }
-      if (counter == users.size()) {
-        users.add(user);
-        return user;
+    for (User us : users) {
+      if (us.getUserName().equals(user.getUserName())) {
+        return null;
       }
     }
-    return null;
+    users.add(user);
+    return user;
   }
 
   public void updateUser(User user) {
