dc39e06004902c4254dc3acbe7b6c6cf7e0c1063
Alexander Lu <alel@stud.ntnu.no>
2020-11-14 21:53:47 +0000

    Fullført checkstyle 2

--- mymovies/restserver/src/main/java/mymovies/restserver/AllUsersApplication.java
+++ mymovies/restserver/src/main/java/mymovies/restserver/AllUsersApplication.java
@@ -2,8 +2,8 @@ package mymovies.restserver;
 
 import com.fasterxml.jackson.databind.Module;
 import mymovies.json.UsersModule;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
